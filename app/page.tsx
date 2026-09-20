db60e7631c518999780083c6201d04448eb6b018
elcidspain <elcidspain@gmail.com>
2026-09-20 16:27:00 +0200

    fix(aeo): render direct-stay facts in the home HTML

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import DirectStayFacts from "@/components/site/DirectStayFacts";
 import FlightLaunch from "@/components/site/FlightLaunch";
 import GuestHome from "@/components/site/GuestHome";
 import SiteFooter from "@/components/site/SiteFooter";
@@ -6,6 +7,7 @@ export default function HomePage() {
   return (
     <>
       <GuestHome />
+      <DirectStayFacts />
       <FlightLaunch />
       <SiteFooter />
     </>
